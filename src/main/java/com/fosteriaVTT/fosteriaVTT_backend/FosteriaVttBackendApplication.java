073ef10b4206b8e929d8d179238b6780691c1cf1
luisemmanuelchavezm <emmanuelchavezzzm@gmail.com>
2026-02-12 17:17:53 +0100

    primer commit

---
+++ src/main/java/com/fosteriaVTT/fosteriaVTT_backend/FosteriaVttBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fosteriaVTT.fosteriaVTT_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FosteriaVttBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FosteriaVttBackendApplication.class, args);
+	}
+
+}
