d17252175be0a26d0b54e42fd9df4410bbfab724
luisemmanuelchavezm <emmanuelchavezzzm@gmail.com>
2026-04-02 03:27:01 +0200

    Commit inicial

---
+++ backend/src/test/java/com/fosteriaVTT/fosteriaVTT_backend/FosteriaVttBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fosteriaVTT.fosteriaVTT_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FosteriaVttBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
