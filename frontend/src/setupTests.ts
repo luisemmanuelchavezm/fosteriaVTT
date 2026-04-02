49925e90d4d8648ea36bb7ecaa6cade78f2157c2
luisemmanuelchavezm <emmanuelchavezzzm@gmail.com>
2026-04-02 21:32:30 +0200

    fix: setuptest and vitest were changed to fix ci

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,11 +1,1 @@
-import { expect, afterEach } from "vitest";
-import { cleanup } from "@testing-library/react";
-import * as matchers from "@testing-library/jest-dom/matchers";
-
-// Añadimos los matchers manualmente de forma segura para Vitest
-expect.extend(matchers);
-
-// Limpia el DOM después de cada test para que no se mezclen
-afterEach(() => {
-  cleanup();
-});
+import "@testing-library/jest-dom";
--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   plugins: [react() as any],
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     globals: true,
-    setupFiles: ['./src/setupTests.ts'],
-    include: ['src/**/*.{test,spec}.{ts,tsx}'],
+    setupFiles: ["./src/setupTests.ts"],
+    include: ["src/**/*.{test,spec}.{ts,tsx}"],
   },
 });
