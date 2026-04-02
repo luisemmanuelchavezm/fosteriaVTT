4e7ae898f9be7b0805aec5d9d706bace4f5e1e3e
luisemmanuelchavezm <emmanuelchavezzzm@gmail.com>
2026-04-02 21:06:48 +0200

    fix: changes into vitests config and the fake test

--- frontend/src/tests/App.test.tsx
+++ frontend/src/tests/App.test.tsx
@@ -1,11 +1,7 @@
 import { describe, it, expect } from "vitest";
 
-describe("Primer test de FosteriaVTT", () => {
-  it("debería sumar 2 + 2 correctamente", () => {
-    expect(2 + 2).toBe(4);
-  });
-
-  it("el entorno de Vitest está configurado", () => {
+describe("App", () => {
+  it("should work", () => {
     expect(true).toBe(true);
   });
 });
--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   plugins: [react() as any],
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/setupTests.ts'],
-    // Aseguramos que busque en la carpeta que creaste
-    include: ['src/**/*.{test,spec}.{ts,tsx}'], 
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
   },
 });
