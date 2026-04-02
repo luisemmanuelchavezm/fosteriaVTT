b289a523c3694d094fafa1e0599e721ce360bee1
luisemmanuelchavezm <emmanuelchavezzzm@gmail.com>
2026-04-02 21:53:48 +0200

    chore: setup test was changed

---
+++ frontend/src/setupTests.ts
@@ -0,0 +1,13 @@
+import { expect, afterEach } from "vitest";
+import { cleanup } from "@testing-library/react";
+import * as matchers from "@testing-library/jest-dom/matchers";
+
+// 1. Extiende los matchers de Vitest con los de Testing Library
+// Esto es lo que permite hacer: expect(...).toBeInTheDocument()
+expect.extend(matchers);
+
+// 2. Limpia el DOM virtual después de cada test
+// Evita que lo que renderices en un test ensucie el siguiente
+afterEach(() => {
+  cleanup();
+});
