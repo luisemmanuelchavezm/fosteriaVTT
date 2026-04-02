6b99c80a65dd1a4c3a35c4a267293ba14cf515ed
luisemmanuelchavezm <emmanuelchavezzzm@gmail.com>
2026-04-02 21:53:15 +0200

    chore: test.yaml was comented until at leat 1 test will be implemented

--- frontend/src/setupTests.ts
+++
@@ -1,5 +0,0 @@
-import { expect } from "vitest";
-import matchers from "@testing-library/jest-dom/matchers";
-
-// safe manual matcher registration for Vitest/jest-dom compatibility
-expect.extend(matchers);
