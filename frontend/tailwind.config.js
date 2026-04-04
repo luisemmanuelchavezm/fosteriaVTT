ca1be29212d92d45d5a93cd8b09b1506b8d7940b
luisemmanuelchavezm <emmanuelchavezzzm@gmail.com>
2026-04-05 00:31:54 +0200

    refactor: prettier was applied on the frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         // Ahora puedes usar la clase 'font-metal'
-        metal: ['"Metal Mania"', 'system-ui'],
+        metal: ['"Metal Mania"', "system-ui"],
       },
     },
   },
   plugins: [],
-}
+};
